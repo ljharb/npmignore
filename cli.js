6c9d7b36b8bd2bd8f3e352d778cd9ce2d96f4ca4
laggingreflex <laggingreflex@gmail.com>
2016-07-04 00:21:15 +0530

    Allow multiple comma separated .gitignore/.npmignore files

--- cli.js
+++ cli.js
@@ -45,6 +45,9 @@ log.inform('updated', dest);
 log.success('  Done.');
 
 function read(fp) {
+  if (fp.indexOf(',') > -1) {
+    return fp.split(/,/g).map(read).join('\n');
+  }
   fp = path.join(process.cwd(), fp);
   if (!fs.existsSync(fp)) {
     return null;
