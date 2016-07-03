c7c314cc987a6c5f06a84d393c06dc2e8eb0a393
laggingreflex <laggingreflex@gmail.com>
2016-07-03 22:57:19 +0530

    Add missing --gitignore --npmignore argument keys

--- cli.js
+++ cli.js
@@ -14,8 +14,8 @@ log.runner = 'npmignore';
  * Find the local `ignore` files we need
  */
 
-var gitignore = argv.g || '.gitignore';
-var npmignore = argv.n || '.npmignore';
+var gitignore = argv.g || argv.gitignore || '.gitignore';
+var npmignore = argv.n || argv.npmignore || '.npmignore';
 
 // optionally specify a different destination
 var dest = argv.d || argv.dest || npmignore;
